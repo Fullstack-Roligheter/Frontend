e5852e18a04e134efdf47d55644586c6eba94d9f
Lachlan Morrison <lmorrison85@gmail.com>
2022-02-15 11:30:39 +0100

    push latest

--- lachlan.js
+++ lachlan.js
@@ -29,54 +29,53 @@ const ListAllBudgetForSpecificUser = () => {
 
             menu.innerHTML =
                 data[i].budgetName;
-menu.appendChild(div)
-            
+            menu.appendChild(div)
+
         }
         body.appendChild(menu);
     }
+}
+//GetBudgets(userId);
 
-    GetBudgets(userId);
-
-    // const GetExpenses = (data) => {
-    // fetch
-    //     ('https://localhost:7073/GetExpenseForSpecificBudget',
-    //         {
-    //             method: 'POST',
-    //             headers: {'Content-type': 'application/json; charset=UTF-8'},
-    //             body: JSON.stringify({ userId: userId, budgetId: budgetId })
-    //         },
-    //     )
-    //     .then(response => response.json()) //.json() is a function that converts the "response" from the fetch into a JSON object
-    //     .then(function (data) {
-    //         appendData(data);
-    //     })
+const GetExpenses = (data) => {
+    fetch
+        ('https://localhost:7073/GetExpenseForSpecificBudget',
+            {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify({ userId: userId, budgetId: budgetId })
+            },
+        )
+        .then(response => response.json()) //.json() is a function that converts the "response" from the fetch into a JSON object
+        .then(function (data) {
+            appendData(data);
+        })
 
 
-    // const appendData = (data) => {
-    //     let body = document.getElementById('app')
+    const appendData = (data) => {
+        let body = document.getElementById('app')
 
-    //     for (var i = 0; i < data.length; i++) 
-    //     {
-    //         let div = document.createElement("div");
+        for (var i = 0; i < data.length; i++) {
+            let div = document.createElement("div");
 
-    //         div.innerHTML =
-    //             'Budget Name : ' + data[i].budgetName + '<br/><br/>' + 'Expenses :' + '<br/>';
+            div.innerHTML =
+                'Budget Name : ' + data[i].budgetName + '<br/><br/>' + 'Expenses :' + '<br/>';
 
-    //             body.appendChild(div);
+            body.appendChild(div);
 
-    //             for (var j = 0; j < data[i].expenses.length; j++) 
-    //             {
-    //                 let div2 = document.createElement("div");
-    //                 div2.innerHTML =
-    //                     'Category Name : ' + data[i].expenses[j].categoryName + '<br/>' +
-    //                     'Amount : ' + data[i].expenses[j].amount + '<br/>' +
-    //                     'Recipient : ' + data[i].expenses[j].recipient + '<br/>' +
-    //                     'Date : ' + data[i].expenses[j].date + '<br/>' +
-    //                     'Comment : ' + data[i].expenses[j].comment + '<br/><br/>'; 
-    //                 body.appendChild(div2);
-    //             }
-    //     }
-    // }
+            for (var j = 0; j < data[i].expenses.length; j++) {
+                let div2 = document.createElement("div");
+                div2.innerHTML =
+                    'Category Name : ' + data[i].expenses[j].categoryName + '<br/>' +
+                    'Amount : ' + data[i].expenses[j].amount + '<br/>' +
+                    'Recipient : ' + data[i].expenses[j].recipient + '<br/>' +
+                    'Date : ' + data[i].expenses[j].date + '<br/>' +
+                    'Comment : ' + data[i].expenses[j].comment + '<br/><br/>';
+                body.appendChild(div2);
+            }
+        }
+    }
 }
 
+GetExpenses();
 ListAllBudgetForSpecificUser();
