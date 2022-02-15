3e1dedaad7b558e56bd304acd0bfaad5313ad91a
Lei <babyonepucca@yahoo.com>
2022-02-15 14:10:16 +0100

    added register code

--- login.js
+++ login.js
@@ -2,8 +2,9 @@
 const registerButton = document.getElementById('register')
 
 const RegisterPage = () => {
-  console.log('Button was clicked')
-  window.location.replace("http://127.0.0.1:5500/Register-an-account.html");
+    console.log('Button was clicked')
+    window.location.replace("file:///C:/Users/babyo/Desktop/Fallproject-frontend-new/Frontend/Register-an-account.html");
+    //den funkar på Leis datorn,men kan behöva ändras när vi mergar
 }
 
 registerButton.addEventListener('click', RegisterPage)
@@ -22,53 +23,49 @@ noLogin.appendChild(noLoginText)
 
 //skapa en eventlistener för logga in knappen
 const SignIn = (e) => {
-  e.preventDefault()
-  //PreventDefault här??
-  //e.stopPropagation() // Ska denna verkligen vara här och om så vad faan gör den??
+    e.preventDefault()
+        //PreventDefault här??
+        //e.stopPropagation() // Ska denna verkligen vara här och om så vad faan gör den??
 
-  console.log('Button2 was clicked')
-  const signInForm = {};
-  signInForm.Username = document.getElementById("Username").value
-  signInForm.Password = document.getElementById("Password").value
-  console.log(signInForm);
-  const jsonStringObj = JSON.stringify(signInForm);
-  debugger
-  //på den skapa en fetch på den för att skicka datan vidare
-  if (signInForm.Username === "" || signInForm.Password === "") {
-    //Om json inte är komplett skicka ut felmeddelande gör texten röd??
-    minDiv.appendChild(felmeddelande)
-  }
-  else {
-    //Om json är komplett gör min fetch här   Få denna att funka, jag vill ha tillbaka en body med true eller false innan jag fortsätter med nästa del.
-    fetch('https://localhost:7073/api/User/login', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: jsonStringObj
-    })
-      .then(resp => {
+    console.log('Button2 was clicked')
+    const signInForm = {};
+    signInForm.Username = document.getElementById("Username").value
+    signInForm.Password = document.getElementById("Password").value
+    console.log(signInForm);
+    const jsonStringObj = JSON.stringify(signInForm);
+    debugger
+    //på den skapa en fetch på den för att skicka datan vidare
+    if (signInForm.Username === "" || signInForm.Password === "") {
+        //Om json inte är komplett skicka ut felmeddelande gör texten röd??
+        minDiv.appendChild(felmeddelande)
+    } else {
+        //Om json är komplett gör min fetch här   Få denna att funka, jag vill ha tillbaka en body med true eller false innan jag fortsätter med nästa del.
+        fetch('https://localhost:7073/api/User/login', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: jsonStringObj
+            })
+            .then(resp => {
 
-        console.log(resp.status)
-        if (resp.status === 401) {
-          //I min fetch ny if sats som skickar en append child som rad 42 med meddelande "Fel lösen eller username" 
-          console.log('No Success')
-          minDiv.appendChild(noLogin)
-        }
-        else {                
-        //eller redirect inloggad sida
-          console.log('Success')
-          return resp.json()
-          .then(data => console.log(data))
-        }
-        //lägga till en ny else if för att fånga andra fel typ 500??
-      })
+                console.log(resp.status)
+                if (resp.status === 401) {
+                    //I min fetch ny if sats som skickar en append child som rad 42 med meddelande "Fel lösen eller username" 
+                    console.log('No Success')
+                    minDiv.appendChild(noLogin)
+                } else {
+                    //eller redirect inloggad sida
+                    console.log('Success')
+                    return resp.json()
+                        .then(data => console.log(data))
+                }
+                //lägga till en ny else if för att fånga andra fel typ 500??
+            })
 
-    
 
-  }
+
+    }
 }
 
 signInButton.addEventListener('click', SignIn)
-
-
