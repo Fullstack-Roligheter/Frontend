244939c8ffe4d552acefcb001362254f00759fa7
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-03-14 11:33:42 +0100

    fix page3 name

--- XPENS/header.js
+++ XPENS/header.js
@@ -1,31 +1,31 @@
-import { changePage } from './router.js'
+import { changePage } from "./router.js";
 
 const registerMenuLi = (id, page) => {
-  const li = document.getElementById(id)
-  li.addEventListener('click', () => {
-    changePage(page)
-    changeColor()
-  })
-}
+  const li = document.getElementById(id);
+  li.addEventListener("click", () => {
+    changePage(page);
+    changeColor();
+  });
+};
 const changeColor = () => {
-  const lis = document.getElementsByTagName('li')
+  const lis = document.getElementsByTagName("li");
   for (let i = 0; i < lis.length; i++) {
     lis[i].onclick = function () {
       for (let i = 0; i < lis.length; i++) {
-        lis[i].style.backgroundColor = ''
+        lis[i].style.backgroundColor = "";
       }
-      this.style.backgroundColor = 'darkcyan'
-    }
+      this.style.backgroundColor = "darkcyan";
+    };
   }
-}
+};
 
 export class Header {
-    constructor() {
-        registerMenuLi('li1', 'home');
-        registerMenuLi('li2', 'checkUser');
-        registerMenuLi('li3', 'page3');
-        registerMenuLi('li4', 'ListAllExpensesInBudget');
-        registerMenuLi('li5', 'filters');
-        registerMenuLi('li6', 'ListAllExpensesInBudgetCat');
-    }
+  constructor() {
+    registerMenuLi("li1", "home");
+    registerMenuLi("li2", "checkUser");
+    registerMenuLi("li3", "addexpense");
+    registerMenuLi("li4", "ListAllExpensesInBudget");
+    registerMenuLi("li5", "filters");
+    registerMenuLi("li6", "ListAllExpensesInBudgetCat");
+  }
 }
--- XPENS/main.js
+++ XPENS/main.js
@@ -1,37 +1,37 @@
 import { render as page1Render } from "./home.js";
 import { render as page2Render } from "./checkUser.js";
-import { render as page3Render } from "./page3.js";
+import { render as page3Render } from "./addexpense.js";
 import { render as ListAllExpensesInBudgetRender } from "./ListAllExpensesInBudget.js";
-import { render as page5Render } from './filters.js';
+import { render as page5Render } from "./filters.js";
 import { render as ListAllExpensesInBudgetCatRender } from "./ListAllExpensesInBudgetCat.js";
 import { registerChangeListener } from "./router.js";
 import { Header } from "./header.js";
 
-const header = new Header()
-const app = document.getElementById('app')
+const header = new Header();
+const app = document.getElementById("app");
 
 registerChangeListener((newPage) => {
-    app.innerHTML = '';
-    switch (newPage) {
-        case 'home':
-            page1Render(app);
-            break;
-        case 'checkUser':
-            page2Render(app);
-            break;
-        case 'page3':
-            page3Render(app);
-            break;
-        case 'ListAllExpensesInBudget':
-            ListAllExpensesInBudgetRender(app);
-            break;
-        case 'filters':
-            page5Render(app)
-            break;
-        case 'ListAllExpensesInBudgetCat':
-            ListAllExpensesInBudgetCatRender(app);
-            break;
-    }
-})
+  app.innerHTML = "";
+  switch (newPage) {
+    case "home":
+      page1Render(app);
+      break;
+    case "checkUser":
+      page2Render(app);
+      break;
+    case "addexpense":
+      page3Render(app);
+      break;
+    case "ListAllExpensesInBudget":
+      ListAllExpensesInBudgetRender(app);
+      break;
+    case "filters":
+      page5Render(app);
+      break;
+    case "ListAllExpensesInBudgetCat":
+      ListAllExpensesInBudgetCatRender(app);
+      break;
+  }
+});
 
-page1Render(app)
+page1Render(app);
