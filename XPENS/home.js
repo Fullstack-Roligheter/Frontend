23f3fe171ea2fad4fc1c7452d46430c0f3ac0c42
Emil Johansson <emil.johansson@studerande-skys.se>
2022-03-15 22:01:15 +0100

    Stylesheet link added to home.js.

--- XPENS/home.js
+++ XPENS/home.js
@@ -4,4 +4,13 @@ export const render = (root) => {
     h1.appendChild(text);
     root.appendChild(h1);
 
+    const head = document.getElementsByTagName('head')[0];
+    let style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css'
+    style.href = 'Style/homeStyle.css';
+    style.media = 'all';
+
+    head.appendChild(style);
+
 }
