f0e92d6f27e4a64d6eb4a1129ff8cb60d48e52c2
Emil Johansson <emil.johansson@studerande-skys.se>
2022-03-21 00:51:07 +0100

    Semi ready

--- XPENS/home.js
+++ XPENS/home.js
@@ -7,24 +7,38 @@ export const render = (root) => {
     style.href = 'Style/homeStyle.css';
     style.media = 'all';
 
+    let contentDiv = document.createElement('div');
+    contentDiv.setAttribute('id', 'contentDiv');
+    let app = document.getElementById('app');
+    app.appendChild(contentDiv);
+
     head.appendChild(style);
 
-    const CreateDiv = (divId) => {
-        let app = document.getElementById('app');
+    const CreateDiv = (divId, inputText, inputType) => {
         
         let newDiv = document.createElement('div');
-        newDiv.setAttribute('id', divId)
+        
+        if(inputText && !inputType) {
+            const text = document.createTextNode(inputText);
+            newDiv.appendChild(text);
+        }
+        
+        if(inputText && inputType) {
+            let element = document.createElement(inputType);
+            const text = document.createTextNode(inputText);
+            element.appendChild(text);
+            newDiv.appendChild(element);
+        }
+        
+        newDiv.setAttribute('id', divId);
         newDiv.className = 'gridDiv';
 
-        // const text = document.createTextNode(divClass);
-        
-        // newDiv.appendChild(text);
-        app.appendChild(newDiv);
+        contentDiv.appendChild(newDiv);
     }
-
-    CreateDiv('ItemA');
-    CreateDiv('ItemB');
-    CreateDiv('ItemC');
-    CreateDiv('ItemD');
-
+    let inputText;
+    CreateDiv('ItemA', inputText);
+    CreateDiv('ItemB', inputText);
+    CreateDiv('ItemC', inputText);
+    CreateDiv('ItemD', inputText);
+    CreateDiv('ItemE', 'Hello world!', 'h1');
 }
