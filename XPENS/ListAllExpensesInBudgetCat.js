48a507df956e70d5ee78314c4a5f77e23940d472
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-04-05 17:30:18 +0200

    comment part of css in order to fix later

--- XPENS/ListAllExpensesInBudgetCat.js
+++ XPENS/ListAllExpensesInBudgetCat.js
@@ -1,115 +1,109 @@
-"use strict";
+'use strict'
 export const render = () => {
-  let userId = 1;
-  let budgetId = "";
-  let tempList = [];
-  let budgetName = "";
+  let userId = 1
+  let budgetId = ''
+  let tempList = []
+  let budgetName = ''
 
-  const oldStyleSheet = document.getElementById("lac-style");
-  if (oldStyleSheet) oldStyleSheet.remove();
+  const oldStyleSheet = document.getElementById('lac-style')
+  if (oldStyleSheet) oldStyleSheet.remove()
 
-  const customStyleSheet = document.createElement("link");
-  customStyleSheet.setAttribute("id", "lac-style");
-  customStyleSheet.setAttribute("rel", "stylesheet");
-  customStyleSheet.setAttribute("href", "lac.css");
+  const customStyleSheet = document.createElement('link')
+  // customStyleSheet.setAttribute("id", "lac-style");
+  // customStyleSheet.setAttribute("rel", "stylesheet");
+  // customStyleSheet.setAttribute("href", "lac.css");
   document.head.appendChild(customStyleSheet)
 
-  const body = document.getElementById("app");
+  const body = document.getElementById('app')
 
-  let pageTitle = document.createElement("h2");
-  pageTitle.innerHTML = "Expenses (Sorted by Category)";
-  body.appendChild(pageTitle);
-
-
+  let pageTitle = document.createElement('h2')
+  pageTitle.innerHTML = 'Expenses (Sorted by Category)'
+  body.appendChild(pageTitle)
 
   const GetBudgets = (loggedInUserId) => {
-    fetch("https://localhost:7073/ListAllBudgetForSpecificUser", {
-      method: "POST",
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+    fetch('https://localhost:7073/ListAllBudgetForSpecificUser', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
       body: JSON.stringify({ userId: loggedInUserId }),
     })
       .then((response) => response.json())
       .then(function (data) {
-        PopulateList(data);
-      });
-  };
+        PopulateList(data)
+      })
+  }
 
   const PopulateList = (data) => {
-    tempList = data;
-    ListAllBudgetInDropdown(data);
-  };
+    tempList = data
+    ListAllBudgetInDropdown(data)
+  }
 
-  const ListAllBudgetInDropdown = (data) => {  
-
-    let menu = document.createElement("select");
-    menu.setAttribute("id", "menu");
+  const ListAllBudgetInDropdown = (data) => {
+    let menu = document.createElement('select')
+    menu.setAttribute('id', 'menu')
 
     for (let i = 0; i < data.length; i++) {
-      let option = document.createElement("option");
-      option.setAttribute("id", "option");
-      option.setAttribute("value", i);
-      option.innerHTML = data[i].budgetName;
-      menu.appendChild(option);
+      let option = document.createElement('option')
+      option.setAttribute('id', 'option')
+      option.setAttribute('value', i)
+      option.innerHTML = data[i].budgetName
+      menu.appendChild(option)
     }
-    body.appendChild(menu);
-    MenuChange();
-    SelectBudgetFromMenu();
-  };
+    body.appendChild(menu)
+    MenuChange()
+    SelectBudgetFromMenu()
+  }
 
   const MenuChange = () => {
-    const menu = document.getElementById("menu");
-    console.log("MenuChange was entered");
+    const menu = document.getElementById('menu')
+    console.log('MenuChange was entered')
     menu.onchange = function () {
-      UpdateName();
-    };
-  };
+      UpdateName()
+    }
+  }
 
   const UpdateName = () => {
-    const tempMenu = document.getElementById("menu");
-    budgetName = tempMenu.options[tempMenu.selectedIndex].text;
+    const tempMenu = document.getElementById('menu')
+    budgetName = tempMenu.options[tempMenu.selectedIndex].text
 
-    SelectBudgetFromMenu();
-  };
+    SelectBudgetFromMenu()
+  }
 
   const SelectBudgetFromMenu = () => {
-    const tempMenu = document.getElementById("menu");
-    budgetName = tempMenu.options[tempMenu.selectedIndex].text;
-    console.log(budgetName);
+    const tempMenu = document.getElementById('menu')
+    budgetName = tempMenu.options[tempMenu.selectedIndex].text
+    console.log(budgetName)
 
     for (let i = 0; i < tempList.length; i++) {
       if (tempList[i].budgetName === budgetName) {
-        budgetId = tempList[i].budgetId;
+        budgetId = tempList[i].budgetId
       }
     }
 
-    GetExpenses();
-  };
+    GetExpenses()
+  }
 
   const GetExpenses = async () => {
     const response = await fetch(
-      "https://localhost:7073/GetExpenseForSpecificBudgetSortedIntoCategories",
+      'https://localhost:7073/GetExpenseForSpecificBudgetSortedIntoCategories',
       {
-        method: "POST",
-        headers: { "Content-type": "application/json; charset=UTF-8" },
+        method: 'POST',
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
         body: JSON.stringify({ userId: userId, budgetId: budgetId }),
       }
-    );
-    const data = await response.json();
-    console.log(data);
+    )
+    const data = await response.json()
+    console.log(data)
 
+    const oldExpensesContainerEl = document.getElementById('expenses-container')
+    if (oldExpensesContainerEl) oldExpensesContainerEl.remove()
 
+    const newExpensesContainerEl = document.createElement('div')
+    newExpensesContainerEl.setAttribute('id', 'expenses-container')
 
-    const oldExpensesContainerEl =
-      document.getElementById("expenses-container");
-    if (oldExpensesContainerEl) oldExpensesContainerEl.remove();
-
-    const newExpensesContainerEl = document.createElement("div");
-    newExpensesContainerEl.setAttribute("id", "expenses-container");
-
-    const date = new Date();
+    const date = new Date()
     // newExpensesContainerEl.innerHTML = date.getMilliseconds();
 
-    body.appendChild(newExpensesContainerEl);
+    body.appendChild(newExpensesContainerEl)
 
     for (let i = 0; i < data.length; i++) {
       // let budgetNameDiv = document.createElement("div");
@@ -120,79 +114,84 @@ export const render = () => {
       // newExpensesContainerEl.appendChild(budgetNameDiv);
 
       for (let k = 0; k < data[i].categories.length; k++) {
+        let categoryContainer = document.createElement('div')
+        categoryContainer.setAttribute('class', 'category-container')
+        newExpensesContainerEl.appendChild(categoryContainer)
 
-
-        let categoryContainer = document.createElement("div");
-        categoryContainer.setAttribute("class", "category-container");
-        newExpensesContainerEl.appendChild(categoryContainer);
-
-        let categoryNameDiv = document.createElement("div");
-        categoryNameDiv.setAttribute("class", "category-name");
+        let categoryNameDiv = document.createElement('div')
+        categoryNameDiv.setAttribute('class', 'category-name')
         categoryNameDiv.innerHTML =
-          "Category Name : " + data[i].categories[k].categoryName
-        categoryContainer.appendChild(categoryNameDiv);
+          'Category Name : ' + data[i].categories[k].categoryName
+        categoryContainer.appendChild(categoryNameDiv)
 
-        let expensesContainerDiv = document.createElement("div");
-        expensesContainerDiv.setAttribute("class", "expense-container");
-        categoryContainer.appendChild(expensesContainerDiv);
+        let expensesContainerDiv = document.createElement('div')
+        expensesContainerDiv.setAttribute('class', 'expense-container')
+        categoryContainer.appendChild(expensesContainerDiv)
 
-            let expenseRowHeaderDiv = document.createElement("div");
-            expenseRowHeaderDiv.setAttribute("class", "expense-row-header");
-            expensesContainerDiv.appendChild(expenseRowHeaderDiv);
+        let expenseRowHeaderDiv = document.createElement('div')
+        expenseRowHeaderDiv.setAttribute('class', 'expense-row-header')
+        expensesContainerDiv.appendChild(expenseRowHeaderDiv)
 
-                let expensesDate = document.createElement("div");
-                expensesDate.setAttribute("class", "expense-item expense-date");
-                expensesDate.innerHTML = 'Date'
-                expenseRowHeaderDiv.appendChild(expensesDate);
+        let expensesDate = document.createElement('div')
+        expensesDate.setAttribute('class', 'expense-item expense-date')
+        expensesDate.innerHTML = 'Date'
+        expenseRowHeaderDiv.appendChild(expensesDate)
 
-                let expensesRecipient = document.createElement("div");
-                expensesRecipient.setAttribute("class", "expense-item expense-recipient");
-                expensesRecipient.innerHTML = 'Recipient'
-                expenseRowHeaderDiv.appendChild(expensesRecipient);
+        let expensesRecipient = document.createElement('div')
+        expensesRecipient.setAttribute(
+          'class',
+          'expense-item expense-recipient'
+        )
+        expensesRecipient.innerHTML = 'Recipient'
+        expenseRowHeaderDiv.appendChild(expensesRecipient)
 
-                let expensesAmount = document.createElement("div");
-                expensesAmount.setAttribute("class", "expense-item expense-amount");
-                expensesAmount.innerHTML = 'Amount';
-                expenseRowHeaderDiv.appendChild(expensesAmount);
+        let expensesAmount = document.createElement('div')
+        expensesAmount.setAttribute('class', 'expense-item expense-amount')
+        expensesAmount.innerHTML = 'Amount'
+        expenseRowHeaderDiv.appendChild(expensesAmount)
 
-                let expensesComment = document.createElement("div");
-                expensesComment.setAttribute("class", "expense-item expense-comment");        
-                expensesComment.innerHTML = 'Comment';
-                expenseRowHeaderDiv.appendChild(expensesComment);
+        let expensesComment = document.createElement('div')
+        expensesComment.setAttribute('class', 'expense-item expense-comment')
+        expensesComment.innerHTML = 'Comment'
+        expenseRowHeaderDiv.appendChild(expensesComment)
 
         for (let j = 0; j < data[i].categories[k].expenses.length; j++) {
+          let expenseRowDiv = document.createElement('div')
+          expenseRowDiv.setAttribute('class', 'expense-row')
+          expensesContainerDiv.appendChild(expenseRowDiv)
 
-          let expenseRowDiv = document.createElement("div");
-          expenseRowDiv.setAttribute("class", "expense-row");
-          expensesContainerDiv.appendChild(expenseRowDiv);
-
-          let expensesDate = document.createElement("div");
-          expensesDate.setAttribute("class", "expense-item expense-date");
+          let expensesDate = document.createElement('div')
+          expensesDate.setAttribute('class', 'expense-item expense-date')
           expensesDate.innerHTML = data[i].categories[k].expenses[j].date
-          expenseRowDiv.appendChild(expensesDate);
+          expenseRowDiv.appendChild(expensesDate)
 
-          let expensesRecipient = document.createElement("div");
-          expensesRecipient.setAttribute("class", "expense-item expense-recipient");
-          expensesRecipient.innerHTML = data[i].categories[k].expenses[j].recipient
-          expenseRowDiv.appendChild(expensesRecipient);
+          let expensesRecipient = document.createElement('div')
+          expensesRecipient.setAttribute(
+            'class',
+            'expense-item expense-recipient'
+          )
+          expensesRecipient.innerHTML =
+            data[i].categories[k].expenses[j].recipient
+          expenseRowDiv.appendChild(expensesRecipient)
 
-          let expensesAmount = document.createElement("div");
-          expensesAmount.setAttribute("class", "expense-item expense-amount");
-          expensesAmount.innerHTML = data[i].categories[k].expenses[j].amount + ' kr';
-          expenseRowDiv.appendChild(expensesAmount);
+          let expensesAmount = document.createElement('div')
+          expensesAmount.setAttribute('class', 'expense-item expense-amount')
+          expensesAmount.innerHTML =
+            data[i].categories[k].expenses[j].amount + ' kr'
+          expenseRowDiv.appendChild(expensesAmount)
 
-          let expensesComment = document.createElement("div");
-          expensesComment.setAttribute("class", "expense-item expense-comment");        
+          let expensesComment = document.createElement('div')
+          expensesComment.setAttribute('class', 'expense-item expense-comment')
           if (!data[i].categories[k].expenses[j].comment) {
-            expensesComment.innerHTML = '';
+            expensesComment.innerHTML = ''
+          } else {
+            expensesComment.innerHTML =
+              data[i].categories[k].expenses[j].comment
           }
-          else {
-            expensesComment.innerHTML = data[i].categories[k].expenses[j].comment
-          }
-          expenseRowDiv.appendChild(expensesComment);
+          expenseRowDiv.appendChild(expensesComment)
         }
       }
     }
-  };
-  GetBudgets(userId);
-};
+  }
+  GetBudgets(userId)
+}
