fa6742d441c652497845b4eb4baf647e1ccc5e78
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-04-05 14:32:06 +0200

    styling forms and clean part of index styling

--- XPENS/reUse.js
+++ XPENS/reUse.js
@@ -6,26 +6,27 @@ export function CreateFormElement(
   inputName,
   formName
 ) {
-  const label = document.createElement(elementName)
-  label.textContent = labelTextContent
-  label.setAttribute('for', forAttribute)
+  const label = document.createElement(elementName);
+  // label.textContent = labelTextContent
+  // label.setAttribute('for', forAttribute)
 
-  const input = document.createElement('input')
-  input.type = inputType
-  input.id = forAttribute
-  input.name = inputName
-  input.setAttribute('required', '')
+  const input = document.createElement("input");
+  input.type = inputType;
+  input.id = forAttribute;
+  input.placeholder = labelTextContent;
+  input.name = inputName;
+  input.setAttribute("required", "");
 
-  formName.append(label)
-  formName.append(input)
+  // formName.append(label)
+  formName.append(input);
 }
 
 export function CreateFormButton(buttonTextContent, buttonClassName, formName) {
-  const button = document.createElement('button')
-  button.textContent = buttonTextContent
-  button.type = 'submit'
-  button.classList.add(buttonClassName)
-  formName.append(button)
+  const button = document.createElement("button");
+  button.textContent = buttonTextContent;
+  button.type = "submit";
+  button.classList.add(buttonClassName);
+  formName.append(button);
 }
 
 export function CreateFormDropDownElement(
@@ -33,86 +34,72 @@ export function CreateFormDropDownElement(
   forAttribute,
   formName
 ) {
-  const label = document.createElement('label')
-  label.textContent = labelTextContent
-  label.setAttribute('for', forAttribute)
+  // const label = document.createElement('label')
+  // label.textContent = labelTextContent
+  // label.setAttribute('for', forAttribute)
 
-  const selectElement = document.createElement('select')
-  selectElement.id = forAttribute
+  const selectElement = document.createElement("select");
+  selectElement.id = forAttribute;
 
-  formName.append(label)
-  formName.append(selectElement)
+  // formName.append(label)
+  formName.append(selectElement);
 }
 
 export function clearForms() {
   for (let i = 0; i < document.forms.length; i++) {
-    document.forms[i].reset()
+    document.forms[i].reset();
   }
 }
 
 //Table functions
 
 export function CreateTable(data) {
-  const app = document.querySelector('#app')
+  const app = document.querySelector("#app");
 
-  const filterContainer = document.querySelector('.filterContainer')
-  const table = document.createElement('table')
-  table.classList.add('table')
+  const filterContainer = document.querySelector(".filterContainer");
+  const table = document.createElement("table");
+  table.classList.add("table");
 
-  const thead = document.createElement('thead')
-  const trHead = document.createElement('tr')
+  const thead = document.createElement("thead");
+  const trHead = document.createElement("tr");
 
-  const thHeadValue = ['Recipient', 'Amount', 'Date', 'Comment']
-  if (thead != ' ') {
+  const thHeadValue = ["Recipient", "Amount", "Date", "Comment"];
+  if (thead != " ") {
     for (let i = 0; i < thHeadValue.length; i++) {
-      const thHeadCell = document.createElement('th')
-      thHeadCell.innerText = thHeadValue[i]
-      trHead.append(thHeadCell)
+      const thHeadCell = document.createElement("th");
+      thHeadCell.innerText = thHeadValue[i];
+      trHead.append(thHeadCell);
     }
   }
 
-  thead.append(trHead)
-  table.append(thead)
-  const tbody = document.createElement('tbody')
-  tbody.classList.add('.table-data')
+  thead.append(trHead);
+  table.append(thead);
+  const tbody = document.createElement("tbody");
+  tbody.classList.add(".table-data");
 
-  const trbody = document.createElement('tr')
+  const trbody = document.createElement("tr");
 
-  let dataHtml = ' '
+  let dataHtml = " ";
   data.forEach((element) => {
     dataHtml = `
       <td >${element.expenseRecipient}</td>
       <td>${element.expenseAmount}</td>
       <td>${element.expenseDate}</td>
       <td>${element.expenseComment}</td>
-  `
-  })
+  `;
+  });
 
-  trbody.innerHTML = dataHtml
-  tbody.append(trbody)
-  table.append(tbody)
+  trbody.innerHTML = dataHtml;
+  tbody.append(trbody);
+  table.append(tbody);
 
-  filterContainer.append(table)
-  app.append(filterContainer)
+  filterContainer.append(table);
+  app.append(filterContainer);
 }
 
-// function CreateTable2(data) {
-//   const tbl = document.querySelector('.table-data')
-//   let dataHtml = ' '
-//   for (data of data) {
-//     dataHtml += `<tr>
-//                     <td>${data.expenseRecipient}</td>
-//                     <td>${data.expenseAmount}</td>
-//                     <td>${data.expenseDate}</td>
-//                     <td>${data.expenseComment}</td>
-//                 </tr>`
+// function ClearingBody() {
+//   const tbody = document.querySelector("tbody");
+//   while (tbody.firstChild) {
+//     tbody.removeChild(tbody.firstChild);
 //   }
-//   tbl.innerHTML = dataHtml
 // }
-
-function ClearingBody() {
-  const tbody = document.querySelector('tbody')
-  while (tbody.firstChild) {
-    tbody.removeChild(tbody.firstChild)
-  }
-}
--- XPENS/savingPlan.js
+++ XPENS/savingPlan.js
@@ -1,98 +1,78 @@
 export const render = (app) => {
-    const div = document.createElement('div');
-    div.classList.add('container', 'saving');
-    const form = document.createElement('form');
-    form.setAttribute('id', 'myForm');
-    const label = document.createElement('LABEL');
-    label.textContent = "Title";
-    label.setAttribute('for', 'title');
-    const inputTitle = document.createElement('input');
-    inputTitle.setAttribute('id', 'title');
-    inputTitle.setAttribute('type', 'text');
-    inputTitle.setAttribute('required', '');
-    var br = document.createElement("br");
+  const div = document.createElement("div");
+  div.classList.add("container", "saving");
+  const form = document.createElement("form");
+  form.setAttribute("id", "myForm");
 
-    const label2 = document.createElement('LABEL');
-    label2.textContent = "Amount";
-    label2.setAttribute('for', 'count');
-    const inputTitle2 = document.createElement('input');
-    inputTitle2.setAttribute('id', 'count');
-    inputTitle2.setAttribute('type', 'text');
-    inputTitle2.setAttribute('required', '');
-    var br2 = document.createElement("br");
+  const inputTitle = document.createElement("input");
+  inputTitle.setAttribute("id", "title");
+  inputTitle.setAttribute("type", "text");
+  inputTitle.setAttribute("required", "");
+  inputTitle.placeholder = "Title";
 
-    const label3 = document.createElement('LABEL');
-    label3.textContent = "Start Date";
-    label3.setAttribute('for', 'start');
-    const inputTitle3 = document.createElement('input');
-    inputTitle3.setAttribute('id', 'start');
-    inputTitle3.setAttribute('type', 'date');
-    inputTitle3.setAttribute('required', '');
-    var br3 = document.createElement("br");
+  const inputTitle2 = document.createElement("input");
+  inputTitle2.setAttribute("id", "count");
+  inputTitle2.setAttribute("type", "text");
+  inputTitle2.setAttribute("required", "");
+  inputTitle2.placeholder = "Amount";
 
-    const label4 = document.createElement('LABEL');
-    label4.textContent = "End Date";
-    label4.setAttribute('for', 'end');
-    const inputTitle4 = document.createElement('input');
-    inputTitle4.setAttribute('id', 'end');
-    inputTitle4.setAttribute('type', 'date');
-    inputTitle4.setAttribute('required', '');
-    var br4 = document.createElement("br");
+  const inputTitle3 = document.createElement("input");
+  inputTitle3.setAttribute("id", "start");
+  inputTitle3.setAttribute("type", "date");
+  inputTitle3.setAttribute("required", "");
+  inputTitle3.placeholder = "Start Date";
 
-    var submit = document.createElement("input");
-    submit.setAttribute('type', 'submit');
-    submit.setAttribute('value', 'submit');
-    submit.setAttribute('id', 'submit');
+  const inputTitle4 = document.createElement("input");
+  inputTitle4.setAttribute("id", "end");
+  inputTitle4.setAttribute("type", "date");
+  inputTitle4.setAttribute("required", "");
+  inputTitle4.placeholder = "End Date";
 
+  var submit = document.createElement("button");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("value", "submit");
+  submit.classList.add("form-button");
+  submit.innerText = "submit";
+  submit.setAttribute("id", "submit");
+  form.append(inputTitle);
+  form.append(inputTitle2);
+  form.append(inputTitle3);
+  form.append(inputTitle4);
+  form.appendChild(submit);
+  div.appendChild(form);
+  app.appendChild(div);
 
-    label.appendChild(inputTitle);
-    label2.appendChild(inputTitle2);
-    label3.appendChild(inputTitle3);
-    label4.appendChild(inputTitle4);
-
-    form.appendChild(label);
-    form.appendChild(br);
-    form.appendChild(label2);
-    form.appendChild(br2);
-    form.appendChild(label3);
-    form.appendChild(br3);
-    form.appendChild(label4);
-    form.appendChild(br4);
-    form.appendChild(submit);
-    div.appendChild(form);
-    app.appendChild(div);
-
-    const myForm = document.getElementById('myForm');
-    myForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        fetch('https://localhost:7073/api/saving/addplan', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    userId: 2,
-                    name: form[0].value,
-                    amount: form[1].value,
-                    planStartDate: form[2].value,
-                    planEndDate: form[3].value
-                })
-            })
-            .then(response => {
-                clearForms();
-                if (response.ok === true) {
-                    alert('Added successfully!');
-                }
-                return response.json()
-            })
-            .catch(err => {
-                console.log(err);
-            })
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("https://localhost:7073/api/saving/addplan", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: 2,
+        name: form[0].value,
+        amount: form[1].value,
+        planStartDate: form[2].value,
+        planEndDate: form[3].value,
+      }),
     })
-}
+      .then((response) => {
+        clearForms();
+        if (response.ok === true) {
+          alert("Added successfully!");
+        }
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
 
 function clearForms() {
-    for (let i = 0; i < document.forms.length; i++) {
-        document.forms[i].reset();
-    }
+  for (let i = 0; i < document.forms.length; i++) {
+    document.forms[i].reset();
+  }
 }
